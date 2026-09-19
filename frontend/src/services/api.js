7965d0f0eecc049d614ac719345c8a6efc151785
Nikhilupadhyay08 <unikhil828@gmail.com>
2026-09-19 14:48:59 +0530

    Prepare frontend for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:4000/api";
-
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:4000/api";
 // Handle API responses
 const handleResponse = async (response, defaultMessage) => {
   let data = {};
